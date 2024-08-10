0503c1ad953266383f61c4f11c8748adcba6a592
Tasleem Ahmad Naeem <tasleem.a.n.official@gmail.com>
2024-08-11 00:07:23 +0500

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import ImageCard from "./ImageCard";
+import ImageSearch from "./ImageSearch";
+
+const URL = "45291993-cee186fb9ffe8b2b78e255bca";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("yellow+flowers");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${URL}&q=${term}&image_type=photo&pretty=true`
+      // `https://pixabay.com/api/?key=45291993-cee186fb9ffe8b2b78e255bca&q=yellow+flowers&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log("Error"));
+  }, [term]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="conatiner mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32 text-red-500">
+          Image not found for the query You Entered ! Try something else
+        </h1>
+      )}
+
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading ...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
